8484bf9023099b1e8c63037eff9eaeb83c8f65b3
Abdullah khalid <91344658+abduIIahKhaIid@users.noreply.github.com>
2025-11-03 19:40:41 +0000

    trpc setup

---
+++ src/trpc/routers/_app.ts
@@ -0,0 +1,12 @@
+import { z } from 'zod';
+import { baseProcedure, createTRPCRouter } from '../init';
+export const appRouter = createTRPCRouter({
+  hello: baseProcedure
+    .query((opts) => {
+      return {
+        greeting: `hello world`,
+      };
+    }),
+});
+// export type definition of API
+export type AppRouter = typeof appRouter;
